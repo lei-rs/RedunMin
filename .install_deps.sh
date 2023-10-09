6ab46706ecd432093769b69a8b75ca2cdea8c0ab
Lei <45155667+leifu1128@users.noreply.github.com>
2023-10-08 21:46:49 -0400

    fix deps

--- .install_deps.sh
+++ .install_deps.sh
@@ -6,4 +6,5 @@ sudo apt-get update
 sudo apt-get install -y pkg-config
 sudo apt-get install -y nasm
 
+pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -r requirements.txt
