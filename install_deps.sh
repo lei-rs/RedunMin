fd4f4a802f24400513ac430fa3bdf21cdc3821ef
Lei <45155667+leifu1128@users.noreply.github.com>
2023-10-09 23:48:15 -0400

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -5,15 +5,10 @@ echo "Installing dependencies..."
 sudo apt-get update
 sudo apt-get install -y pkg-config
 sudo apt-get install -y nasm
+sudo apt install python3.10
 
-mkdir -p ~/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-rm -rf ~/miniconda3/miniconda.sh
-~/miniconda3/bin/conda init bash
-
-conda create -y -n main python=3.10
-conda activate main
+echo "alias py=/usr/bin/python3" >> ~/.bashrc
+echo "alias python=/usr/bin/python3" >> ~/.bashrc
 
 pip install jax[tpu] -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -r requirements.txt
